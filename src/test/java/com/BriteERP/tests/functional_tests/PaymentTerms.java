e4d3ad766a1f4a6dd97a3bc34e47a980bb1af970
ekwillig <ekwillig@gmail.com>
2019-02-04 09:44:26 -0500

    removed fake test folder, That I used for trouble shooting

---
+++ src/test/java/com/BriteERP/tests/functional_tests/PaymentTerms.java
@@ -0,0 +1,17 @@
+package com.BriteERP.tests.functional_tests;
+
+import com.BriteERP.utilities.ConfigurationReader;
+import com.BriteERP.utilities.Driver;
+import com.BriteERP.utilities.TestBase;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaymentTerms extends TestBase {
+
+    public PaymentTerms(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+
+}
